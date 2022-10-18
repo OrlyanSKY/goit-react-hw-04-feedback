5e51426caf334085793ea79d7d3608fad41e09f5
Denis Orlyansky <000slim001@gmail.com>
2022-10-18 14:42:27 +0300

    fix

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 export const Container = styled.div`
   max-width: 480px;
+  padding-top: 100px;
   padding-left: 20px;
   padding-right: 20px;
   margin-left: auto;
